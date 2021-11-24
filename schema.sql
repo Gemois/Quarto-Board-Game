2469f122bf00dcd1620537357e3ae02c91340c51
Giorgos Moisidis <g.moisidis97@gmail.com>
2021-11-24 02:07:48 +0200

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,220 +1,239 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.4.21-MariaDB, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 17, 2021 at 08:50 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: adise_quarto_db
+-- ------------------------------------------------------
+-- Server version	10.4.21-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `adise21_quarto_db`
---
-
-DELIMITER $$
---
--- Procedures
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `clean_board` ()  BEGIN
-REPLACE INTO board SELECT * FROM board_empty;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `place_piece` (`X` TINYINT(1), `Y` TINYINT(1), `piece_id` INT)  BEGIN
-UPDATE board   
-SET piece =piece_id WHERE x=X AND y=Y;
-UPDATE pieces 
-SET available=false WHERE pieces_id=piece_id;
-END$$
-
-DELIMITER ;
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `board`
 --
 
+DROP TABLE IF EXISTS `board`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `board` (
   `x` tinyint(1) NOT NULL,
   `y` tinyint(1) NOT NULL,
-  `piece` int(11) DEFAULT NULL
+  `piece` int(11) DEFAULT NULL,
+  PRIMARY KEY (`x`,`y`),
+  KEY `piece` (`piece`),
+  CONSTRAINT `board_ibfk_1` FOREIGN KEY (`piece`) REFERENCES `pieces` (`pieces_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `board`
+--
+
+LOCK TABLES `board` WRITE;
+/*!40000 ALTER TABLE `board` DISABLE KEYS */;
+INSERT INTO `board` VALUES (1,1,NULL),(1,2,NULL),(1,3,NULL),(1,4,NULL),(2,1,NULL),(2,2,NULL),(2,3,NULL),(2,4,NULL),(3,1,NULL),(3,2,NULL),(3,3,NULL),(3,4,NULL),(4,1,NULL),(4,2,NULL),(4,3,NULL),(4,4,NULL);
+/*!40000 ALTER TABLE `board` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `board_empty`
 --
 
+DROP TABLE IF EXISTS `board_empty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `board_empty` (
   `x` tinyint(1) NOT NULL,
   `y` tinyint(1) NOT NULL,
-  `piece` int(11) DEFAULT NULL
+  `piece` int(11) DEFAULT NULL,
+  PRIMARY KEY (`x`,`y`),
+  KEY `piece` (`piece`),
+  CONSTRAINT `board_empty_ibfk_1` FOREIGN KEY (`piece`) REFERENCES `pieces` (`pieces_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `board_empty`
+--
+
+LOCK TABLES `board_empty` WRITE;
+/*!40000 ALTER TABLE `board_empty` DISABLE KEYS */;
+INSERT INTO `board_empty` VALUES (1,1,NULL),(1,2,NULL),(1,3,NULL),(1,4,NULL),(2,1,NULL),(2,2,NULL),(2,3,NULL),(2,4,NULL),(3,1,NULL),(3,2,NULL),(3,3,NULL),(3,4,NULL),(4,1,NULL),(4,2,NULL),(4,3,NULL),(4,4,NULL);
+/*!40000 ALTER TABLE `board_empty` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `game_status`
 --
 
+DROP TABLE IF EXISTS `game_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `game_status` (
   `status` enum('not active','initialized','started','\nended','aborded') NOT NULL DEFAULT 'not active',
   `p_turn` int(11) DEFAULT NULL,
+  `current_piece` int(11) DEFAULT NULL,
   `result` enum('B','W','D') DEFAULT NULL,
-  `last_change` timestamp NULL DEFAULT NULL
+  `last_change` timestamp NULL DEFAULT NULL,
+  KEY `p_turn` (`p_turn`),
+  CONSTRAINT `game_status_ibfk_1` FOREIGN KEY (`p_turn`) REFERENCES `players` (`player_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `game_status`
+--
+
+LOCK TABLES `game_status` WRITE;
+/*!40000 ALTER TABLE `game_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game_status` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `pieces`
 --
 
+DROP TABLE IF EXISTS `pieces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pieces` (
-  `pieces_id` int(11) NOT NULL,
+  `pieces_id` int(11) NOT NULL AUTO_INCREMENT,
   `is_light` tinyint(1) NOT NULL,
   `is_round` tinyint(1) NOT NULL,
   `is_short` tinyint(1) NOT NULL,
   `is_solid` tinyint(1) NOT NULL,
   `available` tinyint(1) DEFAULT 1,
-  `last_action` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `last_action` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`pieces_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `pieces`
+--
+
+LOCK TABLES `pieces` WRITE;
+/*!40000 ALTER TABLE `pieces` DISABLE KEYS */;
+INSERT INTO `pieces` VALUES (1,1,0,0,1,1,NULL),(2,1,0,0,0,1,NULL),(3,1,0,1,1,1,NULL),(4,1,0,1,0,1,NULL),(5,1,1,0,1,1,NULL),(6,1,1,0,0,1,NULL),(7,1,1,1,1,1,NULL),(8,1,1,1,0,1,NULL),(9,0,0,0,1,1,NULL),(10,0,0,0,0,1,NULL),(11,0,0,1,1,1,NULL),(12,0,0,1,0,1,NULL),(13,0,1,0,1,1,NULL),(14,0,1,0,0,1,NULL),(15,0,1,1,1,1,NULL),(16,0,1,1,0,1,NULL);
+/*!40000 ALTER TABLE `pieces` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `players`
 --
 
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `players` (
-  `player_id` int(11) NOT NULL,
+  `player_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) DEFAULT NULL,
   `password` varchar(10) NOT NULL,
   `token` varchar(100) DEFAULT NULL,
-  `last_action` timestamp NULL DEFAULT NULL
+  `role` varchar(5) DEFAULT NULL,
+  `last_action` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`player_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `session`
 --
 
+DROP TABLE IF EXISTS `session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `session` (
-  `session_id` int(11) NOT NULL,
+  `session_id` int(11) NOT NULL AUTO_INCREMENT,
   `player1_id` int(11) DEFAULT NULL,
   `player2_id` int(11) DEFAULT NULL,
   `player_turn` varchar(100) DEFAULT NULL,
-  `last_action` timestamp NULL DEFAULT NULL
+  `last_action` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `player1_id` (`player1_id`),
+  KEY `player2_id` (`player2_id`),
+  CONSTRAINT `session_ibfk_1` FOREIGN KEY (`player1_id`) REFERENCES `players` (`player_id`),
+  CONSTRAINT `session_ibfk_2` FOREIGN KEY (`player2_id`) REFERENCES `players` (`player_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `session`
 --
 
---
--- Indexes for table `board`
---
-ALTER TABLE `board`
-  ADD PRIMARY KEY (`x`,`y`),
-  ADD KEY `piece` (`piece`);
+LOCK TABLES `session` WRITE;
+/*!40000 ALTER TABLE `session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `session` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for table `board_empty`
+-- Dumping routines for database 'adise_quarto_db'
 --
-ALTER TABLE `board_empty`
-  ADD PRIMARY KEY (`x`,`y`),
-  ADD KEY `piece` (`piece`);
+/*!50003 DROP PROCEDURE IF EXISTS `clean_board` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `clean_board`()
+BEGIN
+REPLACE INTO board SELECT * FROM board_empty;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `place_piece` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `place_piece`(X TINYINT(1),Y TINYINT(1),piece_id INT)
+BEGIN
+UPDATE board   
+SET piece =piece_id WHERE x=X AND y=Y;
+UPDATE pieces 
+SET available=false WHERE pieces_id=piece_id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `game_status`
---
-ALTER TABLE `game_status`
-  ADD KEY `p_turn` (`p_turn`);
-
---
--- Indexes for table `pieces`
---
-ALTER TABLE `pieces`
-  ADD PRIMARY KEY (`pieces_id`);
-
---
--- Indexes for table `players`
---
-ALTER TABLE `players`
-  ADD PRIMARY KEY (`player_id`);
-
---
--- Indexes for table `session`
---
-ALTER TABLE `session`
-  ADD PRIMARY KEY (`session_id`),
-  ADD KEY `player1_id` (`player1_id`),
-  ADD KEY `player2_id` (`player2_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `pieces`
---
-ALTER TABLE `pieces`
-  MODIFY `pieces_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `players`
---
-ALTER TABLE `players`
-  MODIFY `player_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `session`
---
-ALTER TABLE `session`
-  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `board`
---
-ALTER TABLE `board`
-  ADD CONSTRAINT `board_ibfk_1` FOREIGN KEY (`piece`) REFERENCES `pieces` (`pieces_id`);
-
---
--- Constraints for table `board_empty`
---
-ALTER TABLE `board_empty`
-  ADD CONSTRAINT `board_empty_ibfk_1` FOREIGN KEY (`piece`) REFERENCES `pieces` (`pieces_id`);
-
---
--- Constraints for table `game_status`
---
-ALTER TABLE `game_status`
-  ADD CONSTRAINT `game_status_ibfk_1` FOREIGN KEY (`p_turn`) REFERENCES `players` (`player_id`);
-
---
--- Constraints for table `session`
---
-ALTER TABLE `session`
-  ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`player1_id`) REFERENCES `players` (`player_id`),
-  ADD CONSTRAINT `session_ibfk_2` FOREIGN KEY (`player2_id`) REFERENCES `players` (`player_id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-24  2:06:07
